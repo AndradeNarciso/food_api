27a01e86e0aa95f80b920c21b86cbab8a0620d94
AndradeNarciso <andradegregorio277@gmail.com>
2025-10-03 15:16:43 +0200

    first test is up, and  Mockbean was used

--- src/test/java/com/andrade/food_api/repository/RepositoryFoodsTest.java
+++ src/test/java/com/andrade/food_api/repository/RepositoryFoodsTest.java
@@ -1,11 +1,38 @@
 package com.andrade.food_api.repository;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+
+import com.andrade.food_api.domain.Food;
 
 @DataJpaTest
 class RepositoryFoodsTest {
+    @Autowired
+    private RepositoryFoods foodRepository;
+    @MockBean
+    @SuppressWarnings("unused") // the test required a pageble object, maybe cuz a did not use well at configurer
+    private PageableHandlerMethodArgumentResolver pageHandler;
+
     @Test
-    void test() {
+    @DisplayName("Testing  save food function")
+    void shouldSaveFoodAndReturnFood() {
+        Food foodTest = createFood();
+        Food foodSeved = foodRepository.save(foodTest);
+
+        Assertions.assertNotNull(foodSeved);
+        Assertions.assertNotNull(foodSeved.getId());
+
+    }
+
+    private Food createFood() {
+        return Food.builder()
+                .name("Something")
+                .price(12.32)
+                .build();
     }
 }

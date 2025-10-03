e1500dba00780984bde271ea21ff098a227430f5
AndradeNarciso <andradegregorio277@gmail.com>
2025-10-03 18:05:27 +0200

    pageable issue solved

--- src/main/java/com/andrade/food_api/configurer/FoodConfigurer.java
+++ src/main/java/com/andrade/food_api/configurer/FoodConfigurer.java
@@ -2,16 +2,18 @@ package com.andrade.food_api.configurer;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.AllArgsConstructor;
+
 @Configuration
+@AllArgsConstructor
 public class FoodConfigurer implements WebMvcConfigurer {
-    @Autowired
+
     PageableHandlerMethodArgumentResolver pageHandler;
 
     @Override
--- src/test/java/com/andrade/food_api/repository/RepositoryFoodsTest.java
+++ src/test/java/com/andrade/food_api/repository/RepositoryFoodsTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 
 import com.andrade.food_api.domain.Food;
 
@@ -14,9 +12,6 @@ import com.andrade.food_api.domain.Food;
 class RepositoryFoodsTest {
     @Autowired
     private RepositoryFoods foodRepository;
-    @MockBean
-    @SuppressWarnings("unused") // the test required a pageble object, maybe cuz a did not use well at configurer
-    private PageableHandlerMethodArgumentResolver pageHandler;
 
     @Test
     @DisplayName("Testing  save food function")
